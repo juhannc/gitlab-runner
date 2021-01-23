0acaf8d073b65233180d8799f75ed523a2ca92f3
Johann Lange <johannlange@outlook.de>
2021-01-23 12:10:14 +0100

    Fix variable parsing

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,12 @@ LABEL maintainer="Johann Lange <johannlange@yahoo.de>"
 COPY register.bash /register.bash
 
 # Register the gitlab-runner if not exist
-ENTRYPOINT /register.bash
+ENTRYPOINT /register.bash \
+  --url=$URL \
+  --registration-token=$TOKEN \
+  --docker-image=DOCKER_IMAGE \
+  --description=$DESCRIPTION \
+  --tag-list=$TAGS \
+  --run-untagged=$RUN_UNTAGGED \
+  --locked=$LOCKED \
+  --access-level=$ACCESS_LEVEL
--- register.bash
+++ register.bash
@@ -1,5 +1,40 @@
 #!/bin/bash
 
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --url=*)
+      URL="${1#*=}"
+      ;;
+    --registration-token=*)
+      TOKEN="${1#*=}"
+      ;;
+    --docker-image=*)
+      DOCKER_IMAGE="${1#*=}"
+      ;;
+    --description=*)
+      DESCRIPTION="${1#*=}"
+      ;;
+    --tag-list=*)
+      TAGS="${1#*=}"
+      ;;
+    --run-untagged=*)
+      RUN_UNTAGGED="${1#*=}"
+      ;;
+    --locked=*)
+      LOCKED="${1#*=}"
+      ;;
+    --access-level=*)
+      ACCESS_LEVEL="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
 # Register the gitlab-runner
 if ! grep -q $URL /etc/gitlab-runner/config.toml;
 then
